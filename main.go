79c5f7042b864e008d7401c21460a35705cd4659
divisionone-llc <52173099+divisionone-llc@users.noreply.github.com>
2022-06-03 00:24:13 -0400

    Remove query encoding of redis password

--- main.go
+++ main.go
@@ -109,7 +109,7 @@ func realMain() int {
 	s := redisdump.Host{
 		Host:       c.Host,
 		Port:       c.Port,
-		Password:   url.QueryEscape(redisPassword),
+		Password:   redisPassword,
 		TlsHandler: tlshandler,
 	}
 
